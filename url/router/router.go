3d22d8cb204bda7b224cbdb3231604db4845ae5d
Manuel Adalid Moya <madalid@restorepoint.com>
2022-03-31 20:24:00 +0200

    create url router

---
+++ url/router/router.go
@@ -0,0 +1,122 @@
+package router
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/nerock/urlshort/server"
+	"github.com/nerock/urlshort/url"
+)
+
+type URLService interface {
+	CreateURL(context.Context, string) (string, error)
+	GetURL(context.Context, string) (string, error)
+	DeleteURL(context.Context, string) error
+}
+
+type URLRequest struct {
+	URL string
+}
+
+type URLResponse struct {
+	URL      string
+	ShortURL string
+}
+
+type URLRouter struct {
+	urlSvc URLService
+}
+
+func NewURLRouter(urlSvc URLService) URLRouter {
+	return URLRouter{urlSvc: urlSvc}
+}
+
+func (ur URLRouter) Routes(r *chi.Mux) {
+	r.Get("/{id}", ur.redirectTo)
+	r.Route("/api/url", func(r chi.Router) {
+		r.Post("/", ur.createURL)
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", ur.getURL)
+			r.Delete("/", ur.deleteURL)
+		})
+	})
+}
+
+func (ur URLRouter) redirectTo(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		server.RenderError(w, errors.New("could not read id"), http.StatusBadRequest)
+	}
+
+	longURL, err := ur.urlSvc.GetURL(r.Context(), id)
+	switch {
+	case errors.Is(err, url.ErrNotFound):
+		server.RenderError(w, err, http.StatusNotFound)
+		return
+	case err != nil:
+		server.RenderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
+}
+
+func (ur URLRouter) createURL(w http.ResponseWriter, r *http.Request) {
+	var req URLRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		server.RenderError(w, err, http.StatusBadRequest)
+	}
+
+	shortURL, err := ur.urlSvc.CreateURL(r.Context(), req.URL)
+	switch {
+	case errors.Is(err, url.ErrInvalidURL):
+		server.RenderError(w, err, http.StatusBadRequest)
+		return
+	case err != nil:
+		server.RenderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	server.RenderSuccess(w, URLResponse{req.URL, shortURL}, http.StatusCreated)
+}
+
+func (ur URLRouter) getURL(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		server.RenderError(w, errors.New("could not read id"), http.StatusBadRequest)
+	}
+
+	longURL, err := ur.urlSvc.GetURL(r.Context(), id)
+	switch {
+	case errors.Is(err, url.ErrNotFound):
+		server.RenderError(w, err, http.StatusNotFound)
+		return
+	case err != nil:
+		server.RenderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	server.RenderSuccess(w, URLResponse{longURL, id}, http.StatusCreated)
+}
+
+func (ur URLRouter) deleteURL(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		server.RenderError(w, errors.New("could not read id"), http.StatusBadRequest)
+	}
+
+	err := ur.urlSvc.DeleteURL(r.Context(), id)
+	switch {
+	case errors.Is(err, url.ErrNotFound):
+		server.RenderError(w, err, http.StatusNotFound)
+		return
+	case err != nil:
+		server.RenderError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
